31eba32439ab928a92dbad7fa93d7718417c01b5
Abhishek Sharma <abhishek.sharma_cs22@gla.ac.in>
2024-07-28 11:45:19 +0530

    Incorporate yesterday team section

--- app/mui/component.tsx
+++ app/mui/component.tsx
@@ -1,7 +1,21 @@
 import { TextField } from "@mui/material"
-
+import { Avatar, Box, Stack, Typography } from "@mui/material";
 export const FormX = ()=>{
     return(<>
              <TextField variant="outlined" label="Outlined" className="w-full"/>
         </>)
+}
+
+export const User = ({src}:{src:string})=>{
+    return (
+        <Stack justifyItems={"center"} alignItems={"center"}>
+          <Avatar
+            alt="Remy Sharp"
+            src={src}
+            sx={{ width: 110, height: 110 }}
+          />
+          <Typography variant="h6" className="font-bold">Alber Ziegler</Typography>
+          <Typography className="">Principal ML Engineer</Typography>
+        </Stack>
+      );
 }
--- app/mui/page.tsx
+++ app/mui/page.tsx
@@ -1,10 +1,33 @@
-import { FormX } from "./component";
+import React from "react";
+import { Grid, Typography, Link, Stack, Box } from "@mui/material";
+import {User} from "./component";
 
-export default function Page(){
-    return(
-        <>
-            <h1>Material UI</h1>
-            <FormX/>
-        </>
-    )
-}
+const TeamComponent = () => {
+  return (
+    <>
+    <Box sx={{p:10}}>
+      <h1 className="font-light text-xl">Our team spans time zones, languages, and fields of expertise.</h1>
+      <Stack
+        justifyContent={"space-evenly"}
+        direction="column"
+        sx={{ height: 500, p: 10 }}
+      >
+        <Stack direction={"row"} justifyContent={"space-evenly"}>
+          <User src="https://github.com/xalili.png" />
+          <User src="https://github.com/wattenberger.png" />
+          <User src="https://github.com/colebemis.png" />
+          <User src="https://github.com/davidslater.png" />
+        </Stack>
+        <Stack direction={"row"} justifyContent={"space-evenly"}>
+          <User src="https://github.com/drifkin.png" />
+          <User src="https://github.com/dsyme.png" />
+          <User src="https://github.com/eaftan.png" />
+          <User src="https://github.com/ikaliam.png" />
+        </Stack>
+      </Stack>
+    </Box>
+    </>
+  );
+};
+
+export default TeamComponent;
